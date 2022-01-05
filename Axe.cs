74648dbc57d3a2d5b06634436e846ba7b358adb2
Asus <Asus@DESKTOP-04FAORS>
2022-01-05 19:51:22 +0200

    Add project files.

---
+++ Axe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+
+namespace Lab8_TapoiBianca
+{
+    internal class Axe
+    {
+        public Axe()
+        {
+
+        }
+
+        // desenare obiecte 3D 
+        //Desenează axe XYZ.
+        public void DeseneazaAxe()
+        {
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(75, 0, 0);
+            GL.End();
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 75, 0);
+            GL.End();
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Green);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, 75);
+            GL.End();
+        }
+    }
+}
+
